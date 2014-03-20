c93863c26762e8f39d6bd4eb688fc6b459738695
Patrick Hines <patrickhines95@gmail.com>
2014-03-20 16:46:29 +0100

    Created the Vehicle class.

---
+++ MajProg2Spr2014/src/majprog2spr2014/Vehicle.java
@@ -0,0 +1,23 @@
+/*
+ * ====================================================
+
+ Objective: This program will serve as the superclass of
+ all classes other than Fleet. It will set the framework
+ for all necessary vehicles.
+
+ Developed by: Patrick Hines
+ Began: 3-20-2014
+
+
+ * ====================================================
+
+ */
+package majprog2spr2014;
+
+/**
+ *
+ * @author Patrick Hines
+ */
+public class Vehicle {
+
+}
