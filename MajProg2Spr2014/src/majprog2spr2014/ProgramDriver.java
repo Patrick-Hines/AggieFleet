9407c8d62d87ef7dd78d9085f5de916af8a08810
Patrick Hines <patrickhines95@gmail.com>
2014-03-20 16:37:52 +0100

    Created a driver class to connect all other classes in the program.

---
+++ MajProg2Spr2014/src/majprog2spr2014/ProgramDriver.java
@@ -0,0 +1,22 @@
+/*
+ * ====================================================
+
+ Objective: This program will serve as the hub
+ for all other classes in the project.
+
+ Developed by: Patrick Hines
+ Began: 3-20-2014
+
+
+ * ====================================================
+
+ */
+package majprog2spr2014;
+
+/**
+ *
+ * @author Patrick Hines
+ */
+public class ProgramDriver {
+
+}
