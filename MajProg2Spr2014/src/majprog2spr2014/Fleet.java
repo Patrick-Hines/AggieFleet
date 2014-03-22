9a54e5c15ae2fc47b3fdb66d925c4bf2071fe669
Patrick Hines <patrickhines95@gmail.com>
2014-03-22 16:54:51 +0100

    Completed the loadFleet() method for the Fleet class. Testing of this is still needed.

--- MajProg2Spr2014/src/majprog2spr2014/Fleet.java
+++ MajProg2Spr2014/src/majprog2spr2014/Fleet.java
@@ -14,7 +14,11 @@
  */
 package majprog2spr2014;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -63,6 +67,97 @@ public class Fleet {
      * each property in the Fleet class.
      */
     public void loadFleet(String inputFileName) {
+        try {
+            File data = new File(inputFileName);
+            Scanner dataParse = new Scanner(data);
+
+            while (dataParse.hasNextLine()) {
+
+                if (dataParse.nextLine().equalsIgnoreCase("A")) { //The following data will be for an Automobile.
+
+                    //Data for the 'Vehicle' class.
+                    String make = dataParse.nextLine();
+                    String model = dataParse.nextLine();
+                    String vin = dataParse.nextLine();
+                    int year = Integer.parseInt(dataParse.nextLine());
+
+                    //Data for the 'Automobile' class.
+                    boolean hybrid = Boolean.parseBoolean(dataParse.nextLine());
+                    int maxPassengers = Integer.parseInt(dataParse.nextLine());
+                    float trunkSpace = Float.parseFloat(dataParse.nextLine());
+
+                    //Make a new Automobile object and store it in the fleetList ArrayList
+                    Automobile newAuto = new Automobile(hybrid, maxPassengers, trunkSpace, make, model, vin, year);
+                    fleetList.add(newAuto);
+
+                } else if (dataParse.nextLine().equalsIgnoreCase("V")) { //The following data will be for a Van.
+
+                    //Data for the 'Vehicle' class.
+                    String make = dataParse.nextLine();
+                    String model = dataParse.nextLine();
+                    String vin = dataParse.nextLine();
+                    int year = Integer.parseInt(dataParse.nextLine());
+
+                    //Data for the 'Van' class.
+                    float clearanceHeight = Float.parseFloat(dataParse.nextLine());
+                    int numWindows = Integer.parseInt(dataParse.nextLine());
+
+                    //Make a new Van object and store it in the fleetList ArrayList.
+                    Van newVan = new Van(clearanceHeight, numWindows, make, model, vin, year);
+                    fleetList.add(newVan);
+
+                } else if (dataParse.nextLine().equalsIgnoreCase("P")) { //The following data will be for a PassengerVan.
+
+                    //Data for the 'Vehicle' class.
+                    String make = dataParse.nextLine();
+                    String model = dataParse.nextLine();
+                    String vin = dataParse.nextLine();
+                    int year = Integer.parseInt(dataParse.nextLine());
+
+                    //Data for the 'Van' superclass.
+                    float clearanceHeight = Float.parseFloat(dataParse.nextLine());
+                    int numWindows = Integer.parseInt(dataParse.nextLine());
+
+                    //Data for the 'PassengerVan' class.
+                    int numSeatRows = Integer.parseInt(dataParse.nextLine());
+                    int maxPassengers = Integer.parseInt(dataParse.nextLine());
+                    boolean dvdPlayer = Boolean.parseBoolean(dataParse.nextLine());
+
+                    //Make a new PassengerVan object and store it in the fleetList ArrayList.
+                    PassengerVan newPassengerVan = new PassengerVan(numSeatRows,
+                            maxPassengers, dvdPlayer, clearanceHeight, numWindows,
+                            make, model, vin, year);
+
+                    fleetList.add(newPassengerVan);
+
+                } else if (dataParse.nextLine().equalsIgnoreCase("C")) { //The following data will be for a CargoVan.
+
+                    //Data for the 'Vehicle' class.
+                    String make = dataParse.nextLine();
+                    String model = dataParse.nextLine();
+                    String vin = dataParse.nextLine();
+                    int year = Integer.parseInt(dataParse.nextLine());
+
+                    //Data for the 'Van' superclass.
+                    float clearanceHeight = Float.parseFloat(dataParse.nextLine());
+                    int numWindows = Integer.parseInt(dataParse.nextLine());
+
+                    //Data for the 'CargoVan' class.
+                    float maxLoad = Float.parseFloat(dataParse.nextLine());
+                    float cargoArea = Float.parseFloat(dataParse.nextLine());
+
+                    //Make a new CargoVan object and store it in the fleetList ArrayList.
+                    CargoVan newCargoVan = new CargoVan(maxLoad, cargoArea, clearanceHeight, numWindows,
+                            make, model, vin, year);
+
+                    fleetList.add(newCargoVan);
+
+                }
+            }
+
+        } catch (FileNotFoundException notFound) {
+            JOptionPane.showMessageDialog(null, "Sorry, the file that you have specified can not be found.");
+        }
 
     }
 
@@ -83,7 +178,7 @@ public class Fleet {
      * index.
      */
     public Vehicle getVehicle(int index) {
-        return null;
+        return fleetList.get(index);
     }
 
     /**
@@ -91,7 +186,7 @@ public class Fleet {
      * @return The number of items inside the fleetList ArrayList.
      */
     public int getNumVehicles() {
-        return 0;
+        return fleetList.size();
     }
 
     /**
@@ -104,7 +199,7 @@ public class Fleet {
      *
      */
     public void setVehicle(Vehicle givenVehicle, int index) {
-
+        fleetList.set(index, givenVehicle);
     }
 
     /**
@@ -114,7 +209,7 @@ public class Fleet {
      * Appends the given Vehicle to the end of the fleetList ArrayList.
      */
     public void addVehicle(Vehicle givenVehicle) {
-
+        fleetList.add(givenVehicle);
     }
 
     /**
