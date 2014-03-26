790c779eba65eb658f1af8050f311a86c62852a2
Patrick Hines <patrickhines95@gmail.com>
2014-03-27 00:24:09 +0100

    Functional 'save' button

--- MajProg2Spr2014/src/majprog2spr2014/Interface.java
+++ MajProg2Spr2014/src/majprog2spr2014/Interface.java
@@ -15,9 +15,13 @@ package majprog2spr2014;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 /**
@@ -36,9 +40,13 @@ public class Interface extends JFrame {
 
     JLabel intro;
 
-    public Interface() {
+    Fleet activeFleet;
+
+    public Interface(Fleet givenFleet) {
 
         setLayout(new BorderLayout());
+
+        activeFleet = givenFleet;
 
         introSection = new JPanel();
 
@@ -60,8 +68,49 @@ public class Interface extends JFrame {
 
         introSection.add(intro);
 
+        view.addActionListener(new ButtonCommands());
+        add.addActionListener(new ButtonCommands());
+        save.addActionListener(new ButtonCommands());
+        exit.addActionListener(new ButtonCommands());
+
         add(introSection, BorderLayout.CENTER);
         add(commandButtons, BorderLayout.SOUTH);
 
     }
+
+    private class ButtonCommands implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent eventObject) {
+            JButton btnRef = (JButton) eventObject.getSource();
+
+            if (btnRef.getText().equals("Exit Program")) { //Exit button is clicked
+
+                System.exit(0);
+
+            } else if (btnRef.getText().equals("Save Fleet")) { //Save button is clicked
+
+                JFileChooser fileSelection = new JFileChooser();
+
+                int returnVal = fileSelection.showSaveDialog(null);
+
+                String filePath = fileSelection.getSelectedFile().getAbsolutePath();
+                filePath = filePath.replace("\\", "//");
+
+                if (returnVal == JFileChooser.APPROVE_OPTION) {
+
+                    activeFleet.saveFleet(filePath);
+                    JOptionPane.showMessageDialog(null, "File Saved Successfully.");
+
+                }
+
+            } else if (btnRef.getText().equals("Add to Fleet")) { //Add button is clicked
+                String userOption = JOptionPane.showInputDialog("Please enter the type of vehicle you would like to add:");
+
+            } else if (btnRef.getText().equals("View Fleet")) { //View button is clicked
+
+            }
+        }
+    }
+
 }
